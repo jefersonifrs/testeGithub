758ba063d34699a1ae3097436d8e3a7845339c6c
jefersonifrs <85236154+jefersonifrs@users.noreply.github.com>
2022-02-08 15:21:52 -0300

    Adicionei uma linha Tipo hobby

--- src/com/restinga/ifrs/Main.java
+++ src/com/restinga/ifrs/Main.java
@@ -14,6 +14,7 @@ public class Main {
         System.out.println("O Jeferson mora Canoas");
         System.out.println("Trabalha com programador");
         System.out.println("Empresa: DB e DBpt");
+        System.out.println("Hobby: Esporte");
 
 
     }

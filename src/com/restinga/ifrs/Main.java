3eaf155f0b36903f0f76f9fc1744f608fae9192c
jefersonifrs <85236154+jefersonifrs@users.noreply.github.com>
2022-02-08 12:21:36 -0300

    Coloquei imprimir do nomes

---
+++ src/com/restinga/ifrs/Main.java
@@ -0,0 +1,15 @@
+package com.restinga.ifrs;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Teste de execução");
+
+        System.out.println("Jeferson");
+        System.out.println("Lopes");
+        System.out.println("Eugenio");
+        System.out.println("Jeferson Lopes Eugenio");
+
+    }
+}
